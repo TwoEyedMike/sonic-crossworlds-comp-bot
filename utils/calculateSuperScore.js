1eac930a531e9ae94a7e21967f410120afd654a6
Garma <email@email.com>
2024-09-21 16:07:59 +0200

    removed itemless and battle ranks from the super score calculation

--- utils/calculateSuperScore.js
+++ utils/calculateSuperScore.js
@@ -1,8 +1,6 @@
 const {
   RACE_ITEMS_FFA,
   RACE_ITEMS_DUOS,
-  RACE_ITEMLESS_FFA,
-  BATTLE_FFA,
 } = require('../db/models/lobby');
 
 /**
@@ -14,16 +12,12 @@ const {
 function calculateSuperScore(rank, baseRank = 500) {
   let itemSolosRank = rank[RACE_ITEMS_FFA].rank || baseRank;
   let itemTeamsRank = rank[RACE_ITEMS_DUOS].rank || baseRank;
-  let itemlessRank = rank[RACE_ITEMLESS_FFA].rank || baseRank;
-  let battleRank = rank[BATTLE_FFA].rank || baseRank;
 
   itemSolosRank = parseInt(itemSolosRank, 10);
   itemTeamsRank = parseInt(itemTeamsRank, 10);
-  itemlessRank = parseInt(itemlessRank, 10);
-  battleRank = parseInt(battleRank, 10);
 
   // eslint-disable-next-line max-len
-  return Math.ceil((itemSolosRank + itemTeamsRank + itemlessRank + battleRank) / 4);
+  return Math.ceil((itemSolosRank + itemTeamsRank) / 2);
 }
 
 module.exports = calculateSuperScore;
